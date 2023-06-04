e7147c476b9fc09306f3d55413edff5c5faab194
Ziad Ali <zaneroof@gmail.com>
2023-06-04 06:34:36 +0300

    fixed modal issue of opening all services modals at the same time by adding the state in the map

--- src/components/services/ServicesGrid.js
+++ src/components/services/ServicesGrid.js
@@ -1,11 +1,55 @@
-import React from 'react'
-import { GradientBoxRight, SectionBox, SectionContentBox, SectionTitle } from '../../contexts/ThemeContext'
+import React, {useState} from 'react'
 import { useTheme } from '@emotion/react'
 import { Box, Button, Grid, Paper, Typography } from '@mui/material'
-import Development from "../../images/services/development.png"
-import DigitalMarketing from "../../images/services/digital-marketing.png"
-import CreativeDesign from "../../images/services/creative-design.png"
-import styled from "@emotion/styled";
+
+import PropTypes from 'prop-types';
+
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { ServicesData } from './../../data/ServicesData';
+
+const ServiceModal = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+      padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+      padding: theme.spacing(1),
+    },
+  }));
+  
+  function ServiceModalTitle(props) {
+    const { children, onClose, ...other } = props;
+  
+    return (
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    );
+  }
+  
+  ServiceModalTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+  };
 
 export const ContainerPaper = styled(Paper)(({theme}) => ({
     backgroundColor:"transparent",
@@ -38,70 +82,48 @@ function ServicesGrid() {
         mozBoxShadow: "10px 10px 32px 0px rgba(0,0,0,0.1)",
     }}>
         <Grid container spacing={"1rem"} justifyContent={"center"}>
-            <Grid item xs={12} sm={6} md={4}>
+        {        
+        ServicesData.map((service) => {
+            const [open, setOpen] = useState(false);
+
+            const handleClickOpen = () => {
+              setOpen(true);
+            };
+        
+            const handleClose = () => {
+              setOpen(false);
+            };
+            return(
+            <Grid key={service.id} item xs={12} sm={6} md={4}>
                 <ContainerPaper elevation={1}>
-                    <img src={Development} style={{maxHeight:"190px"}}/>
-                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>Development</Typography>
-                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>The best astonishing apps and websites with high quality which users deserve</Typography>
-                    <Button variant={"contained"} sx={{marginTop:".5rem",}}>More</Button>
+                    <img src={service.img} style={{maxHeight:"190px"}}/>
+                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>{service.title}</Typography>
+                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>{service.desc}</Typography>
+                    <Button onClick={handleClickOpen} variant={"contained"} sx={{marginTop:".5rem",}}>More</Button>
+                    <ServiceModal
+                        onClose={handleClose}
+                        aria-labelledby="customized-dialog-title"
+                        open={open}
+                        >
+                        <ServiceModalTitle id="customized-dialog-title" onClose={handleClose}>
+                            {service.title}
+                        </ServiceModalTitle>
+                        <DialogContent dividers>
+                            <Typography gutterBottom>
+                                {service.content}
+                            </Typography>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleClose}>
+                                Close
+                            </Button>
+                        </DialogActions>
+                    </ServiceModal>
                 </ContainerPaper>
             </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <ContainerPaper elevation={1}>
-                    <img src={DigitalMarketing} style={{maxHeight:"190px"}}/>
-                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>Digital Marketing</Typography>
-                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>Giving our clients the level of service and support they expect in our services.</Typography>
-                    <Button variant={"contained"} sx={{marginTop:".5rem",}}>More</Button>
-                </ContainerPaper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <ContainerPaper elevation={1}>
-                    <img src={CreativeDesign} style={{maxHeight:"190px"}}/>
-                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>Creative Design</Typography>
-                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>We provide the best UI/UX Design, Campaign Designs Animation, & Video production</Typography>
-                    <Button variant={"contained"} sx={{marginTop:".5rem"}}>More</Button>
-                </ContainerPaper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <ContainerPaper elevation={1} sx={{
-                    backgroundColor:"transparent",
-                    backdropFilter:"blur(15px)",
-                    padding:"1rem",
-                    transition:".2s",
-                    borderLeft:"solid 1px rgba(255,255,255,.1)",
-                    borderTop:"solid 1px rgba(255,255,255,.1)",
-                    boxShadow: "10px 10px 32px 0px rgba(0,0,0,0.1)",
-                    webkitBoxShadow: "10px 10px 32px 0px rgba(0,0,0,0.1)",
-                    mozBoxShadow: "10px 10px 32px 0px rgba(0,0,0,0.1)",
-                    "&:hover": {
-                        scale:"1.02",
-                        boxShadow: "10px 10px 32px 0px rgba(0,0,0,0.50)",
-                        webkitBoxShadow: "10px 10px 32px 0px rgba(0,0,0,0.50)",
-                        mozBoxShadow: "10px 10px 32px 0px rgba(0,0,0,0.50)",
-                    },
-                }}>
-                    <img src={CreativeDesign} style={{maxHeight:"190px"}}/>
-                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>Creative Design</Typography>
-                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>We provide the best UI/UX Design, Campaign Designs Animation, & Video production</Typography>
-                    <Button variant={"contained"} sx={{marginTop:".5rem"}}>More</Button>
-                </ContainerPaper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <ContainerPaper elevation={1}>
-                    <img src={CreativeDesign} style={{maxHeight:"190px"}}/>
-                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>Creative Design</Typography>
-                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>We provide the best UI/UX Design, Campaign Designs Animation, & Video production</Typography>
-                    <Button variant={"contained"} sx={{marginTop:".5rem"}}>More</Button>
-                </ContainerPaper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <ContainerPaper elevation={1}>
-                    <img src={CreativeDesign} style={{maxHeight:"190px"}}/>
-                    <Typography component={"p"} fontSize="24px" fontWeight={"bold"} marginTop={"1rem"}>Creative Design</Typography>
-                    <Typography component={"p"} fontSize="12px" fontWeight={"light"} color={theme.palette.text.secondary}>We provide the best UI/UX Design, Campaign Designs Animation, & Video production</Typography>
-                    <Button variant={"contained"} sx={{marginTop:".5rem"}}>More</Button>
-                </ContainerPaper>
-            </Grid>
+            )
+        })
+    }
         </Grid>
     </Box>
   )
---
+++ src/data/ServicesData.js
@@ -0,0 +1,27 @@
+import Development from "../images/services/development.png"
+import CreativeDesign from "../images/services/creative-design.png"
+import DigitalMarketing from "../images/services//digital-marketing.png"
+
+export const ServicesData = [
+    {
+        title:"Development",
+        img:Development,
+        desc:"The best astonishing apps and websites with high quality which users deserve",
+        content: "Transforming ideas into seamless digital experiences is our forte at EKO. Our expert developers harness cutting-edge technologies to build exceptional websites and applications that engage users and drive results. From responsive web design and intuitive user interfaces to robust backend development and seamless integration, we ensure that your digital presence is both aesthetically pleasing and functionally efficient. Trust EKO to turn your vision into reality and deliver top-notch solutions that elevate your online presence.",
+        id:1
+    },
+    {
+        title:"Digital Marketing",
+        img:DigitalMarketing,
+        desc:"Giving our clients the level of service and support they expect in our services.",
+        content: "In the fast-paced digital landscape, EKO empowers businesses to stand out from the crowd with our comprehensive digital marketing services. We understand the significance of a strong online presence and offer tailored strategies to boost your brand's visibility, reach, and engagement. Our skilled marketers leverage the latest tools and techniques to craft compelling content, implement effective SEO strategies, and run targeted advertising campaigns across various platforms. Partner with EKO to unlock the full potential of digital marketing and achieve unparalleled growth for your business.",
+        id:2
+    },
+    {
+        title:"Creative Design",
+        img:CreativeDesign,
+        desc:"We provide the best UI/UX Design, Campaign Designs Animation, & Video production.",
+        content: "At EKO, we bring imagination to life through our creative design services. Our team of talented designers works tirelessly to craft visually stunning and captivating designs that leave a lasting impact. Whether it's logo design, brand identity, or user interface design, we strive to create unique and innovative solutions that perfectly represent your brand and resonate with your target audience. With EKO, prepare to embark on a visually enchanting journey where creativity knows no bounds.",
+        id:3
+    },
+]

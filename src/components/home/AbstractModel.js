d6d5f1cb52e458fceb733f331aa801438c5c00fe
Ziad Ali <zaneroof@gmail.com>
2023-06-12 17:22:08 +0300

    solved the material texture in my  3d model by adding Environment from drei and added some custom style by MeshPhysicalMaterial

--- src/components/home/AbstractModel.js
+++ src/components/home/AbstractModel.js
@@ -7,44 +7,56 @@ Source: https://sketchfab.com/3d-models/abstract-rainbow-translucent-pendant-fd7
 Title: Abstract Rainbow Translucent Pendant
 */
 
-import React, { useRef } from 'react'
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import React, { useMemo, useRef } from 'react'
+import { Environment, MeshDistortMaterial, OrbitControls, useGLTF, useTexture } from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber'
-import material from "../../models/hero-abstract/textures/Material_0_baseColor.png"
-import "../../models/hero-abstract/scene.bin"
+// import "../../models/hero-abstract/scene.bin"
+import { MeshPhysicalMaterial } from 'three'
 
 function Model (props){
-  const texture = useTexture(material)
-  const { nodes, materials } = useGLTF(require('../../models/hero-abstract/scene.gltf'))
-  // const { nodes, materials } = useGLTF(require('../../models/abstractModel.glb'))
-  const groupRef = useRef();
-  useFrame(() => (groupRef.current.rotation.y += 0.003 ))
+  const { nodes, materials } = useGLTF(require('../../models/abstractModel.glb'));
+  const texture = useTexture(require('../../models/Material_0_baseColor.png'));
 
+  const groupRef = useRef();
+  useFrame(() => (groupRef.current.rotation.y -= 0.003 ))
+
+  const customMaterial = useMemo(
+    () =>
+      new MeshPhysicalMaterial({
+        map: texture,
+        opacity: 0.6, // Set the desired opacity value (0 to 1)
+        roughness: 0.1, // Set the desired roughness value (0 to 1)
+        metalness: 0.8, // Set the desired metalness value (0 to 1)
+        transparent: true, // Enable transparency for the material
+      }),
+    [texture]
+  );
 console.log("materials:",materials);
   return (
     <mesh {...props}>
       <group {...props} dispose={null}>
-          <group ref={groupRef} scale={0.030}>
-            <mesh geometry={nodes['Hedra001_Material_#0_0'].geometry} 
-            material={materials.Material_0} 
-            position={[-0.618, 0, 3.478]} 
-            rotation={[-Math.PI / 2, 0, 0]} 
-            scale={1} 
-            />
-          </group>
-          </group>
-          <OrbitControls makeDefault enableZoom={false} minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+        <group ref={groupRef} scale={0.030}>
+          <mesh geometry={nodes['Hedra001_Material_#0_0'].geometry} 
+          // material={materials.Material_0}
+          material={customMaterial} // Use the custom material with modified properties
+          position={[-0.618, 0, 3.478]} 
+          rotation={[-Math.PI / 2, 0, 0]} 
+          scale={1} 
+          />
+        </group>
+      </group>
+      <OrbitControls makeDefault enableZoom={false} minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
     </mesh>
   )
 }
 export function AbstractModel(props) {
-  console.log("props:",props);
 
   return (
     <Canvas>
+      <Environment preset='forest'/>
       <Model/>
     </Canvas>
   )
 }
 
-useGLTF.preload(require('../../models/hero-abstract/scene.gltf'))
+useGLTF.preload(require('../../models/abstractModel.glb'))
